e9d8b9c7c0d43ec04c2b75a0ec3dced2d5df7832
wmc911 <wangmingcheng2016@gmail.com>
2017-09-16 00:32:01 +0800

    在路由上使用regex

--- flask_helloworld.py
+++ flask_helloworld.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from werkzeug.routing import BaseConverter
 from os import path
 from werkzeug.utils import secure_filename
-from flask.ext.script import Manager
+from flask_script import Manager
 
 
 class RegexConverter(BaseConverter):
@@ -17,7 +17,7 @@ class RegexConverter(BaseConverter):
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
 
-manager = Manager(app)  # 用manager包装Flask
+# manager = Manager(app)  # 用manager包装Flask
 
 
 @app.route('/')
@@ -40,14 +40,18 @@ def about():
 
 
 # int float path 路由转换器
-@app.route('/user/<int:user_id>')
+@app.route('/user/int=<int:user_id>')
 def user(user_id):
     return 'User %s' % user_id
 
 
-@app.route('/regex/<regex("[a-z]{3}"):user_reg>')
-def regex(user_reg):
-    return 'User %s' % user_reg
+@app.route('/download/client/<regex(".*"):info>')
+def regex(info):
+    return 'info = %s' % info
+
+@app.route('/download/client/<regex(".+\.tar$"):filename>')
+def regex2(filename):
+    return 'filename = %s' % filename
 
 
 # 多个url指向同一个view
@@ -86,15 +90,15 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
-# 不需要F5自动刷新
-@manager.command
-def dev():
-    from livereload import Server
-    live_server = Server(app.wsgi_app)
-    live_server.watch('**/*.*')
-    live_server.serve(open_url=True)
+# # 不需要F5自动刷新
+# @manager.command
+# def dev():
+#     from livereload import Server
+#     live_server = Server(app.wsgi_app)
+#     live_server.watch('**/*.*')
+#     live_server.serve(open_url=True)
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)  # 不需要重启服务
-    manager.run()  # 在用ext时要用这个启动
+    app.run(debug=True)  # 不需要重启服务
+    # manager.run()  # 在用ext时要用这个启动
